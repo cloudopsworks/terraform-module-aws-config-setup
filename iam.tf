c8d96eae68dfc38595ed7cec6542761fd9d110f1
Cristian Beraha <cristian@cloudops.works>
2025-04-21 16:08:38 -0300

    Add "s3:GetBucketAcl" permission to IAM policy
    This change updates the IAM policy to include the "s3:GetBucketAcl" action. It ensures the necessary permissions are in place for accessing bucket ACL details.

--- iam.tf
+++ iam.tf
@@ -38,7 +38,8 @@ data "aws_iam_policy_document" "config_s3_policy" {
       "s3:PutObject",
       "s3:GetObject",
       "s3:ListBucket",
-      "s3:DeleteObject"
+      "s3:DeleteObject",
+      "s3:GetBucketAcl",
     ]
     resources = [
       "${module.config_bucket.s3_bucket_arn}/*",
