eaa4dc75238a3c0af571e928ca2dd7ecab81492f
Cristian Beraha <cristian@cloudops.works>
2025-05-12 13:14:01 -0300

    Fix override handling for resource_types in main.tf
    Removed the unnecessary `try` function for `resource_types` in recording overrides. This simplifies the logic and ensures the value is handled as required without fallback. The update improves clarity and correctness in override processing.

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_config_configuration_recorder" "this" {
       for_each = try(var.settings.recording.overrides, [])
       content {
         description         = try(recording_mode_override.value.description, null)
-        resource_types      = try(recording_mode_override.value.resource_types, [])
+        resource_types      = recording_mode_override.value.resource_types
         recording_frequency = try(recording_mode_override.value.recording_frequency, "CONTINUOUS")
       }
     }
